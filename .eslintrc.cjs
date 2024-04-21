88b739beda8cb41aecd8133afc4dc9f69b4bd264
Kathleen Garrity <garrityk@bc.edu>
2024-04-21 13:27:37 -0700

    chore: add linter, packages, misc setup changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,23 @@
 module.exports = {
-  root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    'no-trailing-spaces': 'error',
+    'react-refresh/only-export-components': 'warn',
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['@/features/*/*']
+      }
     ],
-  },
-}
+    'prettier/prettier': 'error'
+  }
+};
