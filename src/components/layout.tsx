66bab66dde2394d48bbff90b45375ce0267ad822
Kathleen Garrity <kathleen.garrity223@gmail.com>
2024-10-03 09:45:13 -0400

    feat: add scroll effect & better positioning for l/d mode & top nav

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,44 +1,21 @@
-import { AppBar, Box, CssBaseline, IconButton, Toolbar, Typography } from '@mui/material';
-import { useState } from 'react';
+import { Box, Container, CssBaseline, useTheme } from '@mui/material';
 import { Outlet } from 'react-router-dom';
-import { AnimatedNameButton } from '../features/home/components/animated-name';
-import { AnimatedMenuIcon } from './animated-menu-icon';
-import { Menu } from '../features/menu';
 import {
   AnimatedBackground2,
   AnimatedBackground3,
   AnimatedBackground4,
   AnimatedBackground5
 } from './animated-background';
-import { More, Search } from '@mui/icons-material';
 import { Z_INDICES } from '../constants';
+import { TopNavBar } from './nav-bar';
 
 export const Layout = () => {
-  const [showMenu, setShowMenu] = useState(false);
+  const theme = useTheme();
 
   return (
     <>
       <CssBaseline />
-      <Box>
-        <AppBar position="fixed" sx={{ background: 'transparent', boxShadow: 'none' }}>
-          <Toolbar
-            sx={{
-              mt: 1,
-              ml: 12,
-              mr: 12,
-              '@media all': {
-                pl: 0,
-                pr: 0
-              },
-              justifyContent: 'space-between'
-            }}
-          >
-            <AnimatedNameButton />
-            <AnimatedMenuIcon showMenu={showMenu} onClick={() => setShowMenu(!showMenu)} />
-          </Toolbar>
-        </AppBar>
-      </Box>
-
+      <TopNavBar />
       <Box
         sx={{
           overflow: 'hidden',
@@ -50,50 +27,38 @@ export const Layout = () => {
           left: 0,
           right: 0,
           zIndex: Z_INDICES[0],
-          //   background: '#FFF8DC'
-          background: '#fff7e3'
-          //   background: '#000'
-          //   background: lightColors.offwhite
+
+          background: theme.palette.mode === 'light' ? '#fff7e3' : '#333' // can prob just do the dark for now
+          // background: '#000000' // TODO: change all colors for dark mode or just change background tinge
         }}
       >
         <Box
           sx={{
-            animation: 'show1 100s linear infinite'
+            animation: 'show2 45s linear infinite'
           }}
         >
           <AnimatedBackground2 />
         </Box>
         <Box
           sx={{
-            animation: 'show2 100s linear infinite'
+            animation: 'show2 45s linear infinite',
+            opacity: 0,
+            animationDelay: '22s'
           }}
         >
           <AnimatedBackground3 />
         </Box>
-        <Box sx={{ animation: 'show3 100s linear infinite' }}>
+        <Box sx={{ animation: 'show2 45s linear infinite', opacity: 0, animationDelay: '45s' }}>
           <AnimatedBackground4 />
         </Box>
-        <Box sx={{ animation: 'show4 110s linear infinite' }}>
+        <Box sx={{ animation: 'show2 55s linear infinite', opacity: 0, animationDelay: '60s' }}>
           <AnimatedBackground5 />
         </Box>
       </Box>
-
-      <Box
-        sx={{
-          position: 'fixed',
-          width: '100vw',
-          height: '100vh',
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          overflow: 'scroll'
-        }}
-      >
-        <Menu showMenu={showMenu} setShowMenu={setShowMenu} />
-        <Box sx={{ ml: 12, mr: 12 }}>
+      <Box sx={{ ml: '10%', mr: '10%' }}>
+        <Container maxWidth="md">
           <Outlet />
-        </Box>
+        </Container>
       </Box>
     </>
   );
--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,17 +1,37 @@
-import { AppBar, Box, IconButton, Toolbar, useTheme } from '@mui/material';
-import { useContext, useState } from 'react';
+import { AppBar, Box, IconButton, Toolbar, useTheme, Typography, useMediaQuery, Container } from '@mui/material';
+import { useContext, useEffect, useState } from 'react';
 import { ColorModeContext } from '../main';
 import { AnimatedMenuIcon } from '.';
 import { AnimatedNameButton } from '../features/home/components/animated-name';
 import { Brightness7, Brightness4 } from '@mui/icons-material';
 import { Menu } from '../features/menu';
+import { Menu as MenuIcon, Brightness4 as LightModeIcon, Home as HomeIcon } from '@mui/icons-material';
 
 // want to move the show menu to context
 export const TopNavBar = () => {
+  const theme = useTheme();
+
   const [showMenu, setShowMenu] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
   const colorMode = useContext(ColorModeContext);
 
-  const theme = useTheme();
+  // detect scroll position
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+
+    // cleanup event listener on unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <Box>
@@ -19,21 +39,41 @@ export const TopNavBar = () => {
         <Toolbar
           sx={{
             mt: 1,
-            ml: '5%',
-            mr: '5%',
-            '@media all': {
-              pl: 0,
-              pr: 0
-            },
-            justifyContent: 'space-between'
+            justifyContent: 'space-between',
+            transition: 'all 0.3s ease'
           }}
         >
-          <AnimatedNameButton />
-          {theme.palette.mode} mode
-          <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
-            {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
-          </IconButton>
-          <AnimatedMenuIcon showMenu={showMenu} onClick={() => setShowMenu(!showMenu)} />
+          <Box
+            maxWidth="md"
+            sx={{
+              pl: scrolled ? 0 : '10%',
+              pr: scrolled ? 0 : '10%',
+              transition: 'all 0.3s ease',
+              display: 'flex',
+              justifyContent: 'space-between',
+              width: '100%'
+            }}
+          >
+            <Box>
+              <AnimatedNameButton />
+            </Box>
+
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center'
+              }}
+            >
+              <IconButton
+                sx={{ mr: theme.spacing(0.5) }}
+                onClick={colorMode.toggleColorMode}
+                aria-label="dark-light-mode-toggle"
+              >
+                {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+              </IconButton>
+              <AnimatedMenuIcon showMenu={showMenu} onClick={() => setShowMenu(!showMenu)} />
+            </Box>
+          </Box>
         </Toolbar>
       </AppBar>
       <Menu showMenu={showMenu} setShowMenu={setShowMenu} />
